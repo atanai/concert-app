d01c2b0aefa2e230031d01294203cabf1bfc706a
F <f@Fs-MacBook-Pro.local>
2026-01-29 17:23:12 +0700

    Fix table style

--- frontend/app/admin/history/page.tsx
+++ frontend/app/admin/history/page.tsx
@@ -4,28 +4,24 @@ import { history } from '../../data/mockData';
 
 export default function AdminHistory() {
   return (
-    <div className="bg-white border border-gray-200 rounded-lg p-4 md:p-6 overflow-x-auto">
-      <table className="w-full text-sm md:text-base">
-        <thead>
+    <div>
+      <table className="w-full text-sm border border-gray-300 border-collapse">
+        <thead className="bg-gray-90">
           <tr className="border-b border-gray-300 text-left">
-            <th className="pb-3 px-3 md:px-4 text-gray-700 font-bold">Date time</th>
-            <th className="pb-3 px-3 md:px-4 text-gray-700 font-bold">Username</th>
-            <th className="pb-3 px-3 md:px-4 text-gray-700 font-bold">Concert name</th>
-            <th className="pb-3 px-3 md:px-4 text-gray-700 font-bold">Action</th>
+            <th className="py-3 px-3 md:px-4 text-gray-700 font-bold border">Date time</th>
+            <th className="py-3 px-3 md:px-4 text-gray-700 font-bold border">Username</th>
+            <th className="py-3 px-3 md:px-4 text-gray-700 font-bold border">Concert name</th>
+            <th className="py-3 px-3 md:px-4 text-gray-700 font-bold border">Action</th>
           </tr>
         </thead>
         <tbody>
           {history.map((record) => (
             <tr key={record.id} className="border-b border-gray-200 hover:bg-gray-50">
-              <td className="py-3 px-3 md:px-4 text-gray-700">{record.dateTime}</td>
-              <td className="py-3 px-3 md:px-4 text-gray-700">{record.username}</td>
-              <td className="py-3 px-3 md:px-4 text-gray-700">{record.concertName}</td>
-              <td className="py-3 px-3 md:px-4">
-                <span className={`px-3 py-1 rounded-full text-xs md:text-sm font-medium ${
-                  record.action === 'Cancel'
-                    ? 'bg-red-100 text-red-700'
-                    : 'bg-green-100 text-green-700'
-                }`}>
+              <td className="py-3 px-3 md:px-4 text-gray-700 border">{record.dateTime}</td>
+              <td className="py-3 px-3 md:px-4 text-gray-700 border">{record.username}</td>
+              <td className="py-3 px-3 md:px-4 text-gray-700 border">{record.concertName}</td>
+              <td className="py-3 px-3 md:px-4 text-gray-700 border">
+                <span className={`px-3 py-1 md:px-0 rounded-full text-gray-700 md:text-sm font-medium `}>
                   {record.action}
                 </span>
               </td>
